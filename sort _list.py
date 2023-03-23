5f53c274366a93672abf54e05d7708e1d9ee57e4
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-03-23 12:46:58 +0530

    In these programs we are perform longest string in list and sort a list without using any built-in function and find nearest and upper element of given element

---
+++ sort _list.py
@@ -0,0 +1,25 @@
+n = [1,7,22,5,46,20,2,3]
+c = []
+for i in range(len(n)):
+    m = n[0]
+    for j in n:
+        if j < m:
+            m = j
+    c.append(m)
+    n.remove(m)
+print(c)
+
+
+#Time complexity - 0(n2)
+#space complxity - 0(n)
+
+
+
+
+
+
+
+
+
+
+    
